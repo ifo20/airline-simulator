6f2a825cb0c9da0372cf62e5c65cfe25fccb95aa
ifo20 <iain.olliver@gmail.com>
2022-02-27 11:08:33 +0000

    gunicorn

--- app/db.py
+++ app/db.py
@@ -7,68 +7,59 @@ import timeit
 import psycopg2
 import psycopg2.extras
 
-class ConnectionHolder:
+
+class DatabaseInterface:
 	def __init__(self) -> None:
 		start_ts = timeit.default_timer()
 		self.conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
 		logging.info("TIMER DB connection took %s", timeit.default_timer() - start_ts)
 
-CONN_HOLDER = ConnectionHolder()
-
-def db_connection():
-	return CONN_HOLDER.conn
-
-def cursor():
-	return db_connection().cursor()
+	def cursor(self):
+		return self.conn.cursor()
 
 
-def execute(query: str, *args) -> None:
-	with db_connection().cursor() as cur:
-		cur.execute(query, tuple(args))
+	def execute(self, query: str, *args) -> None:
+		with self.cursor() as cur:
+			cur.execute(query, tuple(args))
 
 
-def fetch_one(query: str, *args) -> Any:
-	with db_connection().cursor() as cur:
-		cur.execute(query, tuple(args))
-		result = cur.fetchone()
-	return result
+	def fetch_one(self, query: str, *args) -> Any:
+		with self.cursor() as cur:
+			cur.execute(query, tuple(args))
+			result = cur.fetchone()
+		return result
 
 
-def fetch_all(query: str, *args) -> List[Any]:
-	with db_connection().cursor() as cur:
-		cur.execute(query, tuple(args))
-		result = cur.fetchall()
-	return result
-
-
-class DatabaseInterface:
-	def __init__(self) -> None:
-		pass
+	def fetch_all(self, query: str, *args) -> List[Any]:
+		with self.cursor() as cur:
+			cur.execute(query, tuple(args))
+			result = cur.fetchall()
+		return result
 
 	def migrate(self):
 		with open("001_initial.sql", "r") as f:
-			cursor().execute(f.read())
+			self.cursor().execute(f.read())
 
 	def get_airports(self) -> List[List[Any]]:
-		return fetch_all("SELECT * FROM airports")
+		return self.fetch_all("SELECT * FROM airports")
 
 	def get_airport_by_code(self, code: str) -> List[Any]:
-		return fetch_one("SELECT * FROM airports WHERE code=%s", code)
+		return self.fetch_one("SELECT * FROM airports WHERE code=%s", code)
 
 	def get_airlines(self) -> List[List[Any]]:
-		return fetch_all("SELECT * FROM airlines")
+		return self.fetch_all("SELECT * FROM airlines")
 
 	def get_airline_by_id(self, airline_id: int) -> List[Any]:
-		return fetch_one("SELECT * FROM airlines WHERE id=%s", airline_id)
+		return self.fetch_one("SELECT * FROM airlines WHERE id=%s", airline_id)
 
 	def get_airline_by_name(self, name: str) -> List[Any]:
-		return fetch_one("SELECT * FROM airlines WHERE name=%s", name)
+		return self.fetch_one("SELECT * FROM airlines WHERE name=%s", name)
 
 	def create_airline(
 		self, name: str, hub_code: str, starting_cash: int, starting_popularity: int
 	) -> List[Any]:
 		logging.info("Inserting airline %s ...", name)
-		[inserted_id] = fetch_one(
+		[inserted_id] = self.fetch_one(
 			"""
 INSERT INTO airlines (name, hub, joined_at, last_login_at, cash, popularity)
 VALUES (%s, %s, now(), now(), %s, %s) RETURNING id
@@ -82,10 +73,10 @@ VALUES (%s, %s, now(), now(), %s, %s) RETURNING id
 		return self.get_airline_by_id(inserted_id)
 
 	def update_last_login_at(self, airline_id: int):
-		return execute("UPDATE airlines SET last_login_at=now() WHERE id=%s", airline_id)
+		return self.execute("UPDATE airlines SET last_login_at=now() WHERE id=%s", airline_id)
 
 	def update_airline(self, airline):
-		return execute(
+		return self.execute(
 			"UPDATE airlines SET cash=%s, popularity=%s WHERE id=%s",
 			airline.cash,
 			airline.popularity,
@@ -93,11 +84,11 @@ VALUES (%s, %s, now(), now(), %s, %s) RETURNING id
 		)
 
 	def update_airline_cash(self, airline_id: int, cash: int):
-		return execute("UPDATE airlines SET cash=%s WHERE id=%s", cash, airline_id)
+		return self.execute("UPDATE airlines SET cash=%s WHERE id=%s", cash, airline_id)
 
 	def list_offered_routes(self, airline_id: int):
 		routes = []
-		for row in fetch_all(
+		for row in self.fetch_all(
 			"SELECT * FROM routes WHERE airline_id=%s AND purchased_at IS NULL", airline_id
 		):
 			routes.append(row)
@@ -105,19 +96,19 @@ VALUES (%s, %s, now(), now(), %s, %s) RETURNING id
 
 	def list_owned_routes(self, airline_id: int) -> List[List[Any]]:
 		routes = []
-		for row in fetch_all(
+		for row in self.fetch_all(
 			"SELECT * FROM routes WHERE airline_id=%s AND purchased_at IS NOT NULL", airline_id
 		):
 			routes.append(row)
 		return routes
 
 	def get_route_by_id(self, route_id: int):
-		return fetch_one("SELECT * FROM routes WHERE id=%s", route_id)
+		return self.fetch_one("SELECT * FROM routes WHERE id=%s", route_id)
 
 	def create_route(
 		self, airline_id: int, origin: str, destination: str, popularity: int, cost: int
 	):
-		return fetch_one(
+		return self.fetch_one(
 			"""
 INSERT INTO routes (airline_id, origin, destination, popularity, cost)
 VALUES (%s, %s, %s, %s, %s) RETURNING *""",
@@ -131,7 +122,7 @@ VALUES (%s, %s, %s, %s, %s) RETURNING *""",
 	def update_route_for_purchase(
 		self, route_id: int, purchased_at: datetime, next_available_at: datetime
 	):
-		return execute(
+		return self.execute(
 			"UPDATE routes SET purchased_at=%s, next_available_at=%s WHERE id=%s",
 			purchased_at,
 			next_available_at,
@@ -139,7 +130,7 @@ VALUES (%s, %s, %s, %s, %s) RETURNING *""",
 		)
 
 	def update_route_for_run(self, route):
-		return execute(
+		return self.execute(
 			"UPDATE routes SET next_available_at=%s, last_run_at=%s, last_resulted_at=%s WHERE id=%s",
 			route.next_available_at,
 			route.last_run_at,
@@ -149,7 +140,7 @@ VALUES (%s, %s, %s, %s, %s) RETURNING *""",
 
 	def list_offered_planes(self, airline_id: int):
 		routes = []
-		for row in fetch_all(
+		for row in self.fetch_all(
 			"SELECT * FROM planes WHERE airline_id=%s AND purchased_at IS NULL", airline_id
 		):
 			routes.append(row)
@@ -157,17 +148,17 @@ VALUES (%s, %s, %s, %s, %s) RETURNING *""",
 
 	def list_owned_planes(self, airline_id: int):
 		routes = []
-		for row in fetch_all(
+		for row in self.fetch_all(
 			"SELECT * FROM planes WHERE airline_id=%s AND purchased_at IS NOT NULL", airline_id
 		):
 			routes.append(row)
 		return routes
 
 	def get_plane_by_id(self, plane_id: int):
-		return fetch_one("SELECT * FROM planes WHERE id=%s", plane_id)
+		return self.fetch_one("SELECT * FROM planes WHERE id=%s", plane_id)
 
 	def create_plane(self, airline_id: int, name: str, max_distance: int, cost: int):
-		return fetch_one(
+		return self.fetch_one(
 			"""
 INSERT INTO planes (airline_id, name, max_distance, cost)
 VALUES (%s, %s, %s, %s) RETURNING *""",
@@ -178,7 +169,7 @@ VALUES (%s, %s, %s, %s) RETURNING *""",
 		)
 
 	def update_plane(self, plane):
-		return execute(
+		return self.execute(
 			"""
 UPDATE planes
 SET purchased_at=%s, health=%s, route_id=%s
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,8 @@
+import logging
+
 from app.main import app as application
+
+logging.basicConfig(level=logging.INFO)
 
 app = application
 if __name__ == "__main__":
